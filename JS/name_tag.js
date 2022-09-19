f94bf53421da65b3c9062a01470ea33b2c2e581d
Micaela Silcher <103141209+MicaelaNicSil@users.noreply.github.com>
2022-09-19 16:15:11 -0300

    Update name_tag.js

--- JS/name_tag.js
+++ JS/name_tag.js
@@ -1,5 +1,5 @@
 //this is only for the title that changes my name
-const words = [ "a Frontend Programmer.", "an 2D Animator.", "a Freelancer."]
+const words = [ "a Frontend Developer.", "a 2D Animator.", "a Freelancer."]
 
 let cursor = gsap.to('.cursor', {opacity:0, ease: "power3.inOut", repeat:-1})
 let masterTl = gsap.timeline({repeat: -1}).pause()
